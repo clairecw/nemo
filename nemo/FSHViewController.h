733f7e8f1b6ce3a1a106de3c291455e48bdd52e7
pinto bean <admin@admins-MacBook-Pro.local>
2014-06-14 00:33:43 -0700

    h checkin

--- nemo/FSHViewController.h
+++ nemo/FSHViewController.h
@@ -9,5 +9,12 @@
 #import <UIKit/UIKit.h>
 
 @interface FSHViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UITextField *vesselID;
+@property (weak, nonatomic) IBOutlet UITextField *engine;
+@property (weak, nonatomic) IBOutlet UITextField *hp;
+@property (weak, nonatomic) IBOutlet UITextField *length;
+@property (weak, nonatomic) IBOutlet UITextField *width;
+@property (weak, nonatomic) IBOutlet UITextField *height;
+@property (weak, nonatomic) IBOutlet UIButton *submit;
 
 @end
