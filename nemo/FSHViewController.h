38af95ab75f2a278502c7819d8f296a82df5dec4
pinto bean <admin@admins-MacBook-Pro.local>
2014-06-14 17:28:01 -0700

    big checkin

--- nemo/FSHViewController.h
+++ nemo/FSHViewController.h
@@ -8,13 +8,19 @@
 
 #import <UIKit/UIKit.h>
 
-@interface FSHViewController : UIViewController
+@interface FSHViewController : UIViewController <UIPickerViewDelegate, UIPickerViewDataSource, UITextFieldDelegate>
 @property (weak, nonatomic) IBOutlet UITextField *vesselID;
-@property (weak, nonatomic) IBOutlet UITextField *engine;
+@property (weak, nonatomic) IBOutlet UITextField *enginein;
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollview;
+
+@property (weak, nonatomic) IBOutlet UIPickerView *engine;
 @property (weak, nonatomic) IBOutlet UITextField *hp;
-@property (weak, nonatomic) IBOutlet UITextField *length;
-@property (weak, nonatomic) IBOutlet UITextField *width;
-@property (weak, nonatomic) IBOutlet UITextField *height;
+@property (weak, nonatomic) IBOutlet UISlider *length;
+@property (weak, nonatomic) IBOutlet UISlider *width;
+@property (weak, nonatomic) IBOutlet UISlider *height;
+
 @property (weak, nonatomic) IBOutlet UIButton *submit;
+- (IBAction)submit:(id)sender;
+- (IBAction)dismisskeyboard:(id)sender;
 
 @end
--- nemo/FSHViewController.m
+++ nemo/FSHViewController.m
@@ -8,7 +8,9 @@
 
 #import "FSHViewController.h"
 
-@interface FSHViewController ()
+@interface FSHViewController (){
+    NSMutableArray *listItems;
+}
 
 @end
 
@@ -17,7 +19,35 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+	/*self.engine.delegate = self;
+    self.engine.dataSource = self;
+    self.engine.showsSelectionIndicator = YES;
+    self.engine.opaque = NO;*/
+    
+    self.enginein.delegate = self;
+    
+    //self.enginein.inputView = pickerView;
+    
+    //pickerView = [[UIPickerView alloc] init];
+    
+    //listItems = [[NSMutableArray alloc] initWithObjects:@"Briggs",@"Johnson",@"YAMAHA",@"Suzuki" ,nil];
+    
+    self.vesselID.delegate = self;
+    self.hp.delegate = self;
+    
+}
+
+-(NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView
+{
+    return 1;
+}
+
+-(NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component{
+    return listItems.count;
+}
+
+-(NSString*)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component{
+    return [listItems objectAtIndex:row];
 }
 
 - (void)didReceiveMemoryWarning
@@ -26,4 +56,20 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    [textField resignFirstResponder];
+    
+    return YES;
+}
+
+
+- (IBAction)submit:(id)sender {
+    NSLog(@"%@",self.vesselID);
+}
+
+
+- (IBAction)dismisskeyboard:(id)sender {
+    [self.view endEditing:YES];
+}
 @end
